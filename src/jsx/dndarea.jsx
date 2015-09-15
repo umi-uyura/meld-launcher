18d32164ab69bf4016d28bac843bb6a62ea18d6e
Umi Uyura <umiuyura@gmail.com>
2015-09-15 18:47:45 +0900

    Material UI v0.11.0対応、プロジェクト構造変更

--- src/jsx/dndarea.jsx
+++ src/jsx/dndarea.jsx
@@ -2,8 +2,8 @@
 
 var React = require('react');
 var mui = require('material-ui');
-var ClassNames = require('classnames');
 
+var Colors = mui.Styles.Colors;
 var Paper = mui.Paper;
 
 var DnDArea = React.createClass({
@@ -35,13 +35,45 @@ var DnDArea = React.createClass({
   },
   render: function() {
     var zDepth = this.state.hover ? 5 : 3;
-    var classArea = ClassNames('dndarea', { 'drag': this.state.hover });
+
+    var styles = {
+      wrap: {
+        display: 'inline-block',
+        width: '40%',
+        height: '150px',
+        margin: '20px 10px 10px 10px'
+      },
+      area: {
+        background: Colors.orangeA200,
+        height: '140px'
+      },
+      area_drag: {
+        background: Colors.orangeA400,
+        height: '140px'
+      },
+      title: {
+        color: Colors.orangeA700,
+        fontSize: '120%',
+        fontWeight: 'bold'
+      },
+      content: {
+        height: '100%',
+        lineHeight: '130px',
+        fontSize: '200%',
+        fontWeight: 'bold'
+      }
+    };
+
+    var styleArea = this.state.hover ? styles.area_drag : styles.area;
+
     return (
-      <div className="dndarea-wrap">
-        <span>{this.props.title}</span>
-        <Paper className={classArea}
-               zDepth={zDepth} circle={true} onDragOver={this.doDragOver} onDragLeave={this.doDragLeave} onDrop={this.doDrop}>
-          <p className="dndarea-content">{this.props.children}</p>
+      <div style={styles.wrap}>
+        <span style={styles.title}>{this.props.title}</span>
+        <Paper style={styleArea}
+               zDepth={zDepth}
+               circle={true}
+               onDragOver={this.doDragOver} onDragLeave={this.doDragLeave} onDrop={this.doDrop}>
+          <p style={styles.content}>{this.props.children}</p>
         </Paper>
       </div>
     );
